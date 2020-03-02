f0b1e4490356310eae93c2a662a4181dc348a3ce
Dan DeBlasio <dan@dandeblasio.com>
2020-03-02 16:58:21 -0700

    Comments

--- SortOfSorted.java
+++ SortOfSorted.java
@@ -1,13 +1,29 @@
 class SortOfSorted{
-  public static void SortOfSort(int[] in) {
+  
+  // takes in an array, and sort of sorts it. 
+  public static void sortOfSort(int[] in) {
+    
+    //maintains the range of unsorted values, 
+    //we know we're done when there is only one location left
     int start = 0;
     int end = in.length-1;
+    
+    //keeps track of how many things we have sorted, 
+    // if count %4 is 0 or 1 we add things to the back of the range, 
+    // if count %4 is 2 or 3 add them to the front
     int count = 0;
+    
+    // base is when only one element is left to sort 
     while(start<end) {
+      
+      //find the max left
       int maxIndex = start;
       for(int i=start;i<=end;i++) {
         if(in[i] > in[maxIndex]) maxIndex = i;
       }
+      
+      // swap the max with the correct location, 
+      // move the associated pointer in by one
       if(count%4 < 2) {
         int temp = in[end];
         in[end] = in[maxIndex];
@@ -19,6 +35,8 @@ class SortOfSorted{
         in[maxIndex] = temp;
         start++;
       }
+      
+      // sorted one more, update the counter
       count++;
     }
   }
--- SortOfSortedTester.java
+++ SortOfSortedTester.java
@@ -9,42 +9,42 @@ class SortOfSortedTester {
   @Test
   public void startSorted() {
     int[] in1 = {1,2,3,4,5,6,7,8};
-    SortOfSorted.SortOfSort(in1);
+    SortOfSorted.sortOfSort(in1);
     assertArrayEquals(new int[]{6,5,2,1,3,4,7,8},in1);
   }
   
   @Test
   public void startRevSorted() {
     int[] in1 = {8,7,6,5,4,3,2,1};
-    SortOfSorted.SortOfSort(in1);
+    SortOfSorted.sortOfSort(in1);
     assertArrayEquals(new int[]{6,5,2,1,3,4,7,8},in1);
   }
   
   @Test
   public void startNegSorted() {
     int[] in1 = {-8,-7,-6,-5,-4,-3,-2,-1};
-    SortOfSorted.SortOfSort(in1);
+    SortOfSorted.sortOfSort(in1);
     assertArrayEquals(new int[]{-3,-4,-7,-8,-6,-5,-2,-1},in1);
   }
   
   @Test
   public void startNegRevSorted() {
     int[] in1 = {-1,-2,-3,-4,-5,-6,-7,-8};
-    SortOfSorted.SortOfSort(in1);
+    SortOfSorted.sortOfSort(in1);
     assertArrayEquals(new int[]{-3,-4,-7,-8,-6,-5,-2,-1},in1);
   }
   
   @Test
   public void allSame() {
     int[] in1 = {1,1,1,1,1,1,1,1,1,1,1};
-    SortOfSorted.SortOfSort(in1);
+    SortOfSorted.sortOfSort(in1);
     assertArrayEquals(new int[]{1,1,1,1,1,1,1,1,1,1,1},in1);
   }
   
   @Test
   public void doubles() {
     int[] in1 = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12};
-    SortOfSorted.SortOfSort(in1);
+    SortOfSorted.sortOfSort(in1);
     assertArrayEquals(new int[]{11,11,9,9,7,7,5,5,3,3,1,1,2,2,4,4,6,6,8,8,10,10,12,12},in1);
   }
 }
